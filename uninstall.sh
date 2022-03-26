1db05a37754dbb2173bd649ac5fa1fd7a88d3442
I-Example-I <94490943+I-Example-I@users.noreply.github.com>
2022-03-26 16:00:30 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -37,7 +37,7 @@ echo -e "\033[35m"
 printf "Deleting the node"
 echo -e "\033[0m"
 
-for((sec=0; sec<3; sec++))
+for((sec=0; sec<4; sec++))
 do
         echo -e "\033[35m"
         printf "."
@@ -49,6 +49,10 @@ do
                 docker compose down
         elif [ $sec -eq 2 ]; then
                 rm -rf $HOME/aptos
+        elif [ $sec -eq 3 ]; then
+                cd
+                rm aptos.sh aptos_identity.sh aptos_renew_seeds.sh 
+                rm -rf $HOME/pathfinder $HOME/snap
         fi
 done
 
